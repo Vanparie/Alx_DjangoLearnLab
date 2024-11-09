fbf784a274a666977b929426607ac5d596a41db3
Davidson Leparie <davidsonleparie@gmail.com>
2024-11-09 20:20:02 +0300

    modified query_samples file

--- django-models/relationship_app/query_samples.py
+++ django-models/relationship_app/query_samples.py
@@ -4,17 +4,10 @@ from relationship_app.models import Author, Book, Library, Librarian
 def books_by_author(author_name):
     try:
         author = Author.objects.get(name=author_name)
+        return Book.objects.filter(author=author)
         
-        return author.books.all()  # Using related_name from Book model
     except Author.DoesNotExist:
         return f"No author found with name: {author_name}"
-
-def books_by_author(Author):  
-    try:
-        author = Author.objects.filter(name=author)  
-        return author.books.all()  # Using related_name from Book model
-    except Author.DoesNotExist:
-        return f"No author found with name: {Author}"
 
 # 2. List all books in a specific library
 def books_in_library(library_name):
