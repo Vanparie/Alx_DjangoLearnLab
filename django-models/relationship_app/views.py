463ecb73c42b712d0f33cc48375e8b3e8bae0e6e
Davidson Leparie <davidsonleparie@gmail.com>
2024-11-10 12:19:47 +0300

    Added role-based views and URL configurations for UserProfile

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 
+from django.contrib.auth.decorators import user_passes_test
 from .models import Book
 from django.views.generic.detail import DetailView
 from .models import Library, Book
@@ -32,3 +33,25 @@ def register(request):
     else:
         form = UserCreationForm()
     return render(request, 'relationship_app/register.html', {'form': form})    
+
+
+def is_admin(user):
+    return user.userprofile.role == 'Admin'
+
+def is_librarian(user):
+    return user.userprofile.role == 'Librarian'
+
+def is_member(user):
+    return user.userprofile.role == 'Member'
+
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')
