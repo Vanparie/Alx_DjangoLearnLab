ffaafce47119af989245dc37d49eb8511dea0364
Davidson Leparie <davidsonleparie@gmail.com>
2024-11-16 17:22:08 +0300

    configured https connections

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -37,6 +37,8 @@ SECURE_HSTS_SECONDS = 31536000  # Enforce HTTPS for one year
 SECURE_HSTS_INCLUDE_SUBDOMAINS = True
 SECURE_HSTS_PRELOAD = True
 
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https') # Indicate to Django that the proxy is forwarding HTTPS requests
+
 ALLOWED_HOSTS = []
 
 
