c93cc4434bfb66f20f954d4139d24c7b02ede189
Davidson Leparie <davidsonleparie@gmail.com>
2024-11-23 15:36:29 +0300

    Develop API endpoint using Django Rest Framework

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 from rest_framework.generics import ListAPIView
+from generics.ListAPIView import ListAPIView
 from .models import Book
 from .serializers import BookSerializer
 
