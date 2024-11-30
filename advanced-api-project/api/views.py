53af18b4e000245b35295821bf103f5643af4c4d
Davidson Leparie <davidsonleparie@gmail.com>
2024-11-30 11:04:27 +0300

    filtering, searching and ordering

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 from rest_framework.generics import ListAPIView, RetrieveAPIView, CreateAPIView, UpdateAPIView, DestroyAPIView
 from rest_framework.filters import SearchFilter, OrderingFilter
+from django_filters.rest_framework import OrderingFilter
 from django_filters.rest_framework import DjangoFilterBackend
 
 from django_filters import rest_framework
