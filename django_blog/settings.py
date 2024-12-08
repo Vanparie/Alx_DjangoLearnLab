f00aeead5409c58753397e3aa5b0571772012162
Davidson Leparie <davidsonleparie@gmail.com>
2024-12-08 18:56:43 +0300

    Initial setup of Django blog project

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog.apps.BlogConfig',          #ensure this is included
+    'blog',          #ensure this is included
 ]
 
 MIDDLEWARE = [
