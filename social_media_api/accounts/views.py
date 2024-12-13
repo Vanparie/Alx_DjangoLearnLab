193774215f5898f12b341d22a17a5c6a3a31a46b
Davidson Leparie <davidsonleparie@gmail.com>
2024-12-13 16:19:06 +0300

    Implementing User Follows and Feed Functionality

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -6,11 +6,11 @@ from rest_framework.authtoken.models import Token
 from rest_framework.permissions import IsAuthenticated
 from django.contrib.auth import authenticate
 from .serializers import UserSerializer
-from rest_framework import status, permissions
+from rest_framework import status, permissions, generics
 from rest_framework.decorators import api_view, permission_classes
 from django.contrib.auth import get_user_model
 
-User = get_user_model()
+CustomUser = get_user_model()
 
 class RegisterView(APIView):
     def post(self, request):
@@ -36,21 +36,39 @@ class LoginView(APIView):
 @permission_classes([permissions.IsAuthenticated])
 def follow_user(request, user_id):
     try:
-        user_to_follow = User.objects.get(id=user_id)
+        user_to_follow = CustomUser.objects.get(id=user_id)
         if user_to_follow == request.user:
             return Response({"error": "You cannot follow yourself."}, status=status.HTTP_400_BAD_REQUEST)
 
         request.user.following.add(user_to_follow)
         return Response({"message": f"You are now following {user_to_follow.username}."}, status=status.HTTP_200_OK)
-    except User.DoesNotExist:
+    except CustomUser.DoesNotExist:
         return Response({"error": "User not found."}, status=status.HTTP_404_NOT_FOUND)
 
 @api_view(['POST'])
 @permission_classes([permissions.IsAuthenticated])
 def unfollow_user(request, user_id):
     try:
-        user_to_unfollow = User.objects.get(id=user_id)
+        user_to_unfollow = CustomUser.objects.get(id=user_id)
         request.user.following.remove(user_to_unfollow)
         return Response({"message": f"You have unfollowed {user_to_unfollow.username}."}, status=status.HTTP_200_OK)
-    except User.DoesNotExist:
+    except CustomUser.DoesNotExist:
         return Response({"error": "User not found."}, status=status.HTTP_404_NOT_FOUND)
+    
+
+
+class UserListView(generics.GenericAPIView):
+    """
+    View to list all users (CustomUser) in the system.
+    """
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]    
+
+
+def get(self, request, *args, **kwargs):
+        """
+        Return a list of all users.
+        """
+        users = CustomUser.objects.all()  # Get all users
+        serializer = self.get_serializer(users, many=True)
+        return Response(serializer.data)    
