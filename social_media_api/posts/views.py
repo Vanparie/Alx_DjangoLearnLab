16560a3542d07c891e916587ebbb0768d66c90c1
Davidson Leparie <davidsonleparie@gmail.com>
2024-12-13 16:24:37 +0300

    Implementing User Follows and Feed Functionality

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -44,10 +44,26 @@ class PostViewSet(viewsets.ModelViewSet):
 from rest_framework import generics, permissions
 from .models import Post
 from .serializers import PostSerializer
+from posts.serializers import PostSerializer
 
 class FeedView(generics.ListAPIView):
     serializer_class = PostSerializer
     permission_classes = [permissions.IsAuthenticated]
+
+
+    def get(self, request, *args, **kwargs):
+        """
+        Return posts from users the current user is following, ordered by creation date.
+        """
+        # Get the users the current user is following
+        following_users = request.user.following.all()
+
+        # Filter posts where the author is in the list of following users
+        posts = Post.objects.filter(author__in=following_users).order_by('-created_at')  # Order by most recent first
+        
+        # Serialize the posts and return the response
+        serializer = self.get_serializer(posts, many=True)
+        return Response(serializer.data)
 
     def get_queryset(self):
         user = self.request.user
