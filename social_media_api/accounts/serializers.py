40bcfcbe09a8de79533823a34ef683d4c9997757
Davidson Leparie <davidsonleparie@gmail.com>
2024-12-13 15:24:55 +0300

    Initial commit - Social Media API setup

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -5,12 +5,26 @@ from rest_framework.authtoken.models import Token
 CustomUser = get_user_model()
 
 class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True, style={'input_type': 'password'})
+    confirm_password = serializers.CharField(write_only=True, required=True, style={'input_type': 'password'})
+
     class Meta:
         model = CustomUser
         fields = ['id', 'username', 'password', 'email', 'bio', 'profile_picture']
         extra_kwargs = {'password': {'write_only': True}}
 
+    def validate(self, data):
+        # Ensure the passwords match
+        if data['password'] != data['confirm_password']:
+            raise serializers.ValidationError({"password": "Passwords must match."})
+        return data    
+
     def create(self, validated_data):
         user = CustomUser.objects.create_user(**validated_data)
         Token.objects.create(user=user)  # Generate a token for the new user
         return user
+
+
+class UserLoginSerializer(serializers.Serializer):
+    username = serializers.CharField(required=True)
+    password = serializers.CharField(write_only=True, style={'input_type': 'password'})
